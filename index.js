ac10073c712768b4b30677a72475c6a719cc9c79
Michael Newman <therealmichaelnewman@gmail.com>
2021-07-30 11:46:47 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+victory.innerHTML = "Michael is the champion";
+
+
